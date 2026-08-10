8048985fe7eda43ed856cd5e845be4287a6964be
Adhamdrazz <157208140+Adhamdrazz@users.noreply.github.com>
2026-08-10 03:52:39 +0300

    Fix POST request body

--- api/water-sources.js
+++ api/water-sources.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
         latitude,
         longitude,
         photo_url
-      } = req.body;
+     } = req.body || {};
 
       if (
         !name ||
